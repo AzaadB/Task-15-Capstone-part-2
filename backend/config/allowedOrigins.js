3367a81cf49f87df7c388c20a990c81eb08d037a
AzaadB <azaadb09@gmail.com>
2023-06-20 12:52:12 +0200

    Summary

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 //Defining our allowedOrigins(line 2), which is equal to am array with an origin inside it(line 3)//
 const allowedOrigins = [
-  "https://zany-car-notes.onrender.com"
+  "https://road-records.onrender.com"
 ];
 //All of these localhosts(lines 4-6), are the ones we would want to access our rest api//
 module.exports = allowedOrigins;
